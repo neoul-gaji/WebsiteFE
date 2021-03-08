ad3c67deb364199ea8e6f59da1ac74b55f70f540
Just4Study <pidaoh@g.skku.edu>
2021-03-08 20:12:06 +0900

    Login Page commit

--- src/views/identity/Login.vue
+++ src/views/identity/Login.vue
@@ -1,8 +1,22 @@
 // 로그인 창
 
 <template lang="">
-    <div>
-       로그인 창 - 서인석
+    <div id="container" class="d-flex-column justify-content-center">
+        <div class="d-inline-flex align-items-center">
+            <img src="@/assets/logo.png" alt="logo" id="logo-img">
+            <h3>꿈꾸는 개발자</h3>
+        </div>
+       <p>새로운 도전, <strong>꿈꾸는 개발자</strong>와 함께</p>
+       <form action="">
+            <input type="text" id="input-text" name="id" placeholder="아이디">
+            <input type="text" id= "input-text" name="id" placeholder="비밀번호"> <br>
+            <b-button>로그인</b-button>
+        </form>
+        <div class="d-flex justify-content-between" style="font-size: 12px;">
+            <label><input type="checkbox"> 로그인 유지</label>
+            <a href="">비밀번호/아이디 찾기</a>
+        </div>
+        <span>새로운 도전을 꿈꾸시나요? </span><a href="/register">회원가입</a>
     </div>
 </template>
 <script>
@@ -10,6 +24,22 @@ export default {
     
 }
 </script>
-<style lang="">
+<style scoped>
+
+
+#logo-img {
+    height: 100px;
+}
+
+#container {
+    position: absolute;
+    top:50%;
+    left:50%;
+    width: 300px;
+    height: 300px;
+    margin-left: -150px;
+    margin-top: -150px;
+    text-align:center
+}
     
 </style>
