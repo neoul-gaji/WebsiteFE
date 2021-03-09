1912a6705a54f59e2e257019205ccd0df3c4bcf0
Just4Study <pidaoh@g.skku.edu>
2021-03-09 13:42:02 +0900

    Login Page commit

--- src/views/identity/Login.vue
+++ src/views/identity/Login.vue
@@ -7,16 +7,20 @@
             <h3>꿈꾸는 개발자</h3>
         </div>
        <p>새로운 도전, <strong>꿈꾸는 개발자</strong>와 함께</p>
-       <form action="">
-            <input type="text" id="input-text" name="id" placeholder="아이디">
-            <input type="text" id= "input-text" name="id" placeholder="비밀번호"> <br>
-            <b-button>로그인</b-button>
+       <form action="" id="form-container">
+            <input type="text" name="id" placeholder="아이디" class="w-100 p-2">
+            <input type="text" name="password" placeholder="비밀번호" class="w-100 p-2">
+            <button type="submit" formmethod="POST" class="w-100 p-2">로그인</button>
         </form>
-        <div class="d-flex justify-content-between" style="font-size: 12px;">
+        <div class="d-flex justify-content-between" id="form-additional-container">
             <label><input type="checkbox"> 로그인 유지</label>
-            <a href="">비밀번호/아이디 찾기</a>
+            <div>
+                <a href="">비밀번호</a>
+                <span> | </span>
+                <a href="">아이디 찾기</a>
+            </div>
         </div>
-        <span>새로운 도전을 꿈꾸시나요? </span><a href="/register">회원가입</a>
+        <strong>새로운 도전을 꿈꾸시나요? </strong><a href="/register">회원가입</a>
     </div>
 </template>
 <script>
@@ -26,6 +30,38 @@ export default {
 </script>
 <style scoped>
 
+input[type="text"]:focus {
+    outline: none;
+}
+
+a {
+    text-decoration: none;
+}
+
+input[type="text"] {
+    border :none;
+    border-bottom: 2px solid #ec4c66;
+    margin-bottom: 5px;
+}
+
+button {
+    background-color: #4b4b68;
+    border: none;
+    border-radius: 7px;
+    color : white;
+}
+
+#form-container {
+    width: 300px;
+}
+
+#form-additional-container {
+    font-size: 12px; 
+    width: 250px; 
+    margin:auto;
+    margin-top: 15px;
+    margin-bottom: 10px;
+}
 
 #logo-img {
     height: 100px;
@@ -36,9 +72,9 @@ export default {
     top:50%;
     left:50%;
     width: 300px;
-    height: 300px;
+    height: 350px;
     margin-left: -150px;
-    margin-top: -150px;
+    margin-top: -175px;
     text-align:center
 }
     
